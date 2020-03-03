2c1ed6bd03eb48946c24518b66cee4aabbb6d2cb
Jake Ash and Uriel Gonzalez <jake@>
2020-03-03 13:41:35 -0800

    Add user authentication

---
+++ client/src/app.config.js
@@ -0,0 +1,6 @@
+export default {
+    url: 'https://dev-476875.okta.com',
+    issuer: 'https://dev-476875.okta.com/oauth2/default',
+    redirect_uri: window.location.origin + '/implicit/callback',
+    client_id: '0oa2z0hpsrCQ8FXWD4x6'
+  };
